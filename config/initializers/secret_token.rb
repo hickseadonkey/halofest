ba4442f2cefcf408883b29ff7eb09e8b9c1e7265
Nick Albino <nick.albino@gmail.com>
2014-01-22 14:07:50 -0800

    rails new halofest --skip-bundle --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Halofest::Application.config.secret_key_base = '4edb0f12d4056679ff60355b268aeb41d0e0670e7c5b5e831f5b4a640b0eb4bee9b254f5c84bde79b029a78af4e90d6e5f2a917e7e513ff7ecd96e995fe06f98'
