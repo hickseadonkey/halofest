29e12f372106619ec7c3c6190c7bbd2eb825c65d
Nick Albino <nick.albino@gmail.com>
2014-01-28 17:33:00 -0800

    roster belongs to user

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Halofest::Application.routes.draw do
   root "home#index"
 
   get "home/index"
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
