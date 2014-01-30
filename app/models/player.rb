418bfa13c5f9998bc57d101702486c2f766b25a0
Nick Albino <nick.albino@gmail.com>
2014-01-30 10:38:10 -0800

    rosters habtm players, players habtm rosters

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ActiveRecord::Base
   has_many :player_stats, class_name: 'PlayerStats'
+  has_and_belongs_to_many :rosters
 
   def full_name
     [first_name, last_name].reject(&:blank?).join(" ")
--- app/models/roster.rb
+++ app/models/roster.rb
@@ -1,5 +1,4 @@
 class Roster < ActiveRecord::Base
   belongs_to :user, :draft
-
-  has_many :players
+  has_many_and_belongs_to_many :players
 end
