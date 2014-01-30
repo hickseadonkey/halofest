3cd3dc0a2f7307369be0d2c19e525facbfb3e9dc
Nick Albino <nick.albino@gmail.com>
2014-01-29 19:55:36 -0800

    removed jointable

--- db/migrate/20140129014238_create_join_table_player_roster_assignments.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTablePlayerRosterAssignments < ActiveRecord::Migration
-  def change
-    create_join_table :players, :rosters do |t|
-       t.index [:player_id, :roster_id]
-      # t.index [:roster_id, :player_id]
-    end
-  end
-end
