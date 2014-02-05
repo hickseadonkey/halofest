766e67d22b03d6a44c576f6bca7da5ecf5a99ae6
Sam Pierson <sam@sampierson.com>
2014-02-05 14:05:27 -0800

    Validate that player has a first name

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ActiveRecord::Base
   has_many :player_stats, class_name: 'PlayerStats'
   has_and_belongs_to_many :rosters
 
+  validates :first_name, presence: true
+
   def full_name
     [first_name, last_name].reject(&:blank?).join(" ")
   end
