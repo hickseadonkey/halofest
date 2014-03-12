b317e3b41b20421192866aa70761691a714c1b0c
Sam Pierson <sam@sampierson.com>
2014-03-11 20:36:07 -0700

    Pull out player stats in PlayersController#index
    then in the view, work with the in-memory stats, instead of going back to the DB

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,7 +4,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @players = Player.includes(:player_stats).all
   end
 
   # GET /players/1
